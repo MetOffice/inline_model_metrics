44c6a0001cf1bb2411a1e38449dc90aa8a74ceca
Jon Seddon <jon.seddon@metoffice.gov.uk>
2022-11-21 14:52:07 +0000

    Added required files fro PyPI

--- inline_model_metrics/__init__.py
+++ inline_model_metrics/__init__.py
@@ -1,5 +1,8 @@
 # (C) British Crown Copyright 2022, Met Office.
 # Please see LICENSE for license details.
+__version__ = '0.1.0'
+
+
 from inline_model_metrics.tempest_atmos_river import TempestExtremesAR
 from inline_model_metrics.tempest_cyclone import TempestExtremesCyclone
 from inline_model_metrics.um_preprocess import UMTempestPreprocess
